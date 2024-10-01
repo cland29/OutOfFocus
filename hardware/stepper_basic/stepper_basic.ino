e5279e51733642f9a3ddba563bf4fc9e35aa0182
cland29 <carl.lee.landskron@gmail.com>
2024-10-01 12:29:39 -0400

    Add stepper and web serial classes

--- hardware/stepper_basic/stepper_basic.ino
+++ hardware/stepper_basic/stepper_basic.ino
@@ -17,16 +17,40 @@ void setup() {
   myStepper.setSpeed(700);  // Adjust this value if needed
 
   // Optional: Begin serial communication for debugging
-  Serial.begin(115200);
-  Serial.println("Stepper Motor Test");
+  Serial.begin(9600);
 }
 
 void loop() {
   // Step forward one full revolution
   // Serial.println("Stepping forward...");
-  myStepper.step(stepsPerRevolution*32);
-  delay(100);  // Wait for a second
+  if (Serial.available()) {
+    String message = Serial.readStringUntil('\n');
+    Serial.println("Received: " + message);
+    if (isInteger(message)){
+      myStepper.step(message.toInt()*32);
+    }else{
+      Serial.println("Please enter an integer value");
+    }
+  }
 
-  myStepper.step(-stepsPerRevolution*32);
-  delay(100);  // Wait for a second
+  //myStepper.step(stepsPerRevolution*32);
+  //delay(100);  // Wait for a second
+
+  //myStepper.step(-stepsPerRevolution*32);
+  //delay(100);  // Wait for a second
+}
+
+bool isInteger(String str) {
+  if (str.length() == 0) {
+    return false;  // Empty string is not an integer
+  }
+
+  for (int i = 0; i < str.length(); i++) {
+    Serial.println(str[i]);
+    if (!isDigit(str[i]) && !(i == 0 && str[i] == 45)) {
+      return false;  // If any character is not a digit, return false
+    }
+  }
+  
+  return true; // If all characters are digits, return true
 }
